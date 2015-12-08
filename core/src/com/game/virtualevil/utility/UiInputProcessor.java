df451508c3046af6c4d4cbbb5630a67f2f84ce45
vs-slavchev <vs_slavchev@abv.bg>
2015-12-08 16:58:05 +0100

    v0.20 - added screenshot capturing with F10;

--- core/src/com/game/virtualevil/utility/UiInputProcessor.java
+++ core/src/com/game/virtualevil/utility/UiInputProcessor.java
@@ -1,7 +1,13 @@
 package com.game.virtualevil.utility;
 
+import java.nio.ByteBuffer;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Input.Keys;
 
 /**
@@ -10,11 +16,16 @@ import com.badlogic.gdx.Input.Keys;
  * @author vs */
 public class UiInputProcessor  implements InputProcessor{
 
+	private static int screenShotCounter = 1;
+	
 	@Override
 	public boolean keyDown(int keycode) {
 		switch (keycode) {
 		case Keys.ESCAPE:
 			Gdx.app.exit();
+			return true;
+		case Keys.F10:
+			saveScreenshot();
 			return true;
 			
 		//examples
@@ -73,5 +84,34 @@ public class UiInputProcessor  implements InputProcessor{
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
+	private static void saveScreenshot() {
+		try {
+			FileHandle fh;
+			do {
+				fh = new FileHandle("screenshots/screenshot" + screenShotCounter++ + ".png");
+			} while (fh.exists());
+			Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(0, 0,
+					Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			
+			/* flip the resulting image as opengl draws the
+			 * opposite way of writing to file */
+			ByteBuffer pixels = pixmap.getPixels();
+            int numBytes = Gdx.graphics.getWidth() * Gdx.graphics.getHeight() * 4;
+            byte[] lines = new byte[numBytes];
+            int numBytesPerLine = Gdx.graphics.getWidth() * 4;
+            for (int i = 0; i < Gdx.graphics.getHeight(); i++) {
+                pixels.position((Gdx.graphics.getHeight() - i - 1) * numBytesPerLine);
+                pixels.get(lines, i * numBytesPerLine, numBytesPerLine);
+            }
+            pixels.clear();
+            pixels.put(lines);
+            pixels.clear();
+			
+			PixmapIO.writePNG(fh, pixmap);
+			pixmap.dispose();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
