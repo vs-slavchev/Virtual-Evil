f8d2b4894f1b2909d002a122f3a8e4f0eb8e73b4
vs-slavchev <vs_slavchev@abv.bg>
2016-06-25 21:00:45 +0200

    Refactor music manager

--- core/src/com/game/virtualevil/utility/asset/MusicManager.java
+++ core/src/com/game/virtualevil/utility/asset/MusicManager.java
@@ -1,97 +1,43 @@
 package com.game.virtualevil.utility.asset;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
 public class MusicManager  {
-	public Music song1, song2, song3, song4, song5, song6, song7;
+	
+	private List<Music> songs = new ArrayList<>();
+	private String[] songNames = {"NARC", "Rose the Wraith",
+			"Star Eater", "Divide", "Skelletons in the attic",
+			"ABIIISMO - Machine Uprising", "Video Stalker" };
+	private int currentSongIndex = 0;
 
 	public void initialize() {
-		song1 = Gdx.audio.newMusic(Gdx.files.internal("NARC.mp3"));
-		song2 = Gdx.audio.newMusic(Gdx.files.internal("Rose the Wraith.mp3"));
-		song3 = Gdx.audio.newMusic(Gdx.files.internal("Star Eater.mp3"));
-		song4 = Gdx.audio.newMusic(Gdx.files.internal("Divide.mp3"));
-		song5 = Gdx.audio.newMusic(Gdx.files.internal("Skelletons in the attic.mp3"));
-		song6 = Gdx.audio.newMusic(Gdx.files.internal("ABIIISMO - Machine Uprising.mp3"));
-		song7 = Gdx.audio.newMusic(Gdx.files.internal("Video Stalker.mp3"));
-
-		song1.setVolume(0.5f);
-		song1.play();
-
-		// This makes song2 play after song1 finishes
-		song1.setOnCompletionListener(new Music.OnCompletionListener() {
-
-			@Override
-			public void onCompletion(Music music) {
-				song2.setVolume(0.5f);
-				song2.play();
-			}
-		});
+		for (String name : songNames){
+			songs.add(Gdx.audio.newMusic(Gdx.files.internal(name + ".mp3")));
+		}
 		
-		song2.setOnCompletionListener(new Music.OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(Music music) {
-				song3.setVolume(0.5f);
-				song3.play();
-				
-			}
-		});
+		for (int i = 0; i < songs.size(); i++){
+			songs.get(i).setVolume(0.5f);
+			songs.get(i).setOnCompletionListener(music -> playNextSong());
+		}
 		
-		song3.setOnCompletionListener(new Music.OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(Music music) {
-				song4.setVolume(0.5f);
-				song4.play();
-				
-			}
-		});
-		
-		song4.setOnCompletionListener(new Music.OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(Music music) {
-				song5.setVolume(0.5f);
-				song5.play();
-				
-			}
-		});
-		
-		song5.setOnCompletionListener(new Music.OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(Music music) {
-				song6.setVolume(0.5f);
-				song6.play();
-				
-			}
-		});
-		
-		song6.setOnCompletionListener(new Music.OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(Music music) {
-				song7.setVolume(0.5f);
-				song7.play();
-				
-			}
-		});
-		
-		song7.setOnCompletionListener(new Music.OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(Music music) {
-				song1.setVolume(0.5f);
-				song1.play();
-				
-			}
-		});
+		songs.get(0).play();
+	}
+	
+	private void playNextSong(){
+		currentSongIndex++;
+		if (currentSongIndex >= songs.size()){
+			currentSongIndex = 0;
+		}
+		songs.get(currentSongIndex).play();
 	}
 
 	public void dispose() {
-		song1.dispose();
-		song2.dispose();
-		song3.dispose();
+		for (Music song : songs){
+			song.dispose();
+		}
 	}
 }
